d51f08f77cd8ded75ec0f1f3b596ddafde5e4c22
Squoshy-boop <sebascreeper@gmail.com>
2021-09-06 17:25:26 -0300

    Parches ejercicio 2

--- Ejercicio2.aspx.cs
+++ Ejercicio2.aspx.cs
@@ -16,7 +16,7 @@ namespace TP4_PROG3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            MostrarTodo();
         }
 
        public DataSet Consulta(string consulta)
@@ -34,100 +34,113 @@ namespace TP4_PROG3
         protected void btnFiltrar_Click(object sender, EventArgs e)
         {
             bool aux = false;
+
+            if (txtProducto.Text.Length == 0 && txtProducto.Text.Length == 0)
+            {
+                MostrarTodo();
+            }
+
+            else if (txtProducto.Text.Length != 0 && txtProducto.Text.Length != 0)
+            {     
+                    if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 0)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 1)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 2)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+
+                    if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 0)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 1)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text) + "and IdCategoría >" + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 2)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto >" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+
+                    if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 0)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 1)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 2)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+
+                    aux = true;
+                
+                if (txtProducto.Text.Length != 0)
+                {
+                    if (int.Parse(ddlProducto.SelectedValue) == 0 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto = " + int.Parse(txtProducto.Text));
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 1 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text));
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlProducto.SelectedValue) == 2 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdProducto < " + int.Parse(txtProducto.Text));
+                        grdProductos.DataBind();
+                    }
+                    txtProducto.Text = "";
+                }
+                if (txtCategoria.Text.Length != 0)
+                {
+                    if (int.Parse(ddlCategoria.SelectedValue) == 0 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlCategoria.SelectedValue) == 1 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    if (int.Parse(ddlCategoria.SelectedValue) == 2 && aux == false)
+                    {
+                        grdProductos.DataSource = Consulta("select * from Productos where IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
+                        grdProductos.DataBind();
+                    }
+                    txtCategoria.Text = "";
+                }
+            }
+
             
-            if (txtProducto.Text.Length != 0 && txtProducto.Text.Length != 0)
-            {
-                if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 0)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 1)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 0 && int.Parse(ddlCategoria.SelectedValue) == 2)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto =" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-
-                if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 0)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 1)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text) + "and IdCategoría >" + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 1 && int.Parse(ddlCategoria.SelectedValue) == 2)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto >" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-
-                if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 0)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 1)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 2 && int.Parse(ddlCategoria.SelectedValue) == 2)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto <" + int.Parse(txtProducto.Text) + "and IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-
-                aux = true;
-            }
-            if(txtProducto.Text.Length != 0)
-            {
-                if(int.Parse(ddlProducto.SelectedValue) == 0 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto = " + int.Parse(txtProducto.Text));
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 1 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto > " + int.Parse(txtProducto.Text));
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlProducto.SelectedValue) == 2 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdProducto < " + int.Parse(txtProducto.Text));
-                    grdProductos.DataBind();
-                }
-                txtProducto.Text = "";
-            }
-            if(txtCategoria.Text.Length != 0)
-            {
-                if(int.Parse(ddlCategoria.SelectedValue) == 0 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdCategoría = " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlCategoria.SelectedValue) == 1 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdCategoría > " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                if (int.Parse(ddlCategoria.SelectedValue) == 2 && aux == false)
-                {
-                    grdProductos.DataSource = Consulta("select * from Productos where IdCategoría < " + int.Parse(txtCategoria.Text) + "order by IdCategoría");
-                    grdProductos.DataBind();
-                }
-                txtCategoria.Text = "";
-            }
         }
 
         protected void btnBorrar_Click(object sender, EventArgs e)
+        {
+            MostrarTodo();
+        }
+
+        private void MostrarTodo()
         {
             SqlConnection cn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security=True");
             cn.Open();
