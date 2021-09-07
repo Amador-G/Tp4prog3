28d68a76e036a84d2c70ae05ceac1a0a3311c843
Sebas-Re <32151264+Squoshy-Boop@users.noreply.github.com>
2021-09-06 21:13:52 -0300

    Add files via upload

---
+++ Ejercicio2.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio2.aspx.cs" Inherits="TP4_PROG3.Ejercicio2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Id Producto:
+            <asp:DropDownList ID="ddlProducto" runat="server">
+                <asp:ListItem Value="0">Igual a:</asp:ListItem>
+                <asp:ListItem Value="1">Mayor a:</asp:ListItem>
+                <asp:ListItem Value="2">Menor a:</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtProducto" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Id Categoria:
+            <asp:DropDownList ID="ddlCategoria" runat="server">
+                <asp:ListItem Value="0">Igual a:</asp:ListItem>
+                <asp:ListItem Value="1">Mayor a:</asp:ListItem>
+                <asp:ListItem Value="2">Menor a:</asp:ListItem>
+            </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtCategoria" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnFiltrar" runat="server" OnClick="btnFiltrar_Click" Text="FIltrar" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnBorrar" runat="server" OnClick="btnBorrar_Click" Text="Quitar filtro" />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+&nbsp;<asp:GridView ID="grdProductos" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Ejercicio2.aspx.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.ComponentModel.DataAnnotations;
+
+namespace TP4_PROG3
+{
+    public partial class Ejercicio2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+            string consulta = "select * from Productos ";
+
+
+            DataSet ds = new DataSet();
+            SqlConnection cn = new SqlConnection(rutaBd);
+            cn.Open();
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+            adaptador.Fill(ds, "Productos");
+            grdProductos.DataSource = ds.Tables["Productos"];
+            grdProductos.DataBind();
+            cn.Close();
+
+        }
+
+        protected void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            if (txtProducto.Text=="" && txtCategoria.Text == "") 
+            {
+                string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+
+                string consulta = "select * from Productos ";
+
+
+                DataSet ds = new DataSet();
+                SqlConnection cn = new SqlConnection(rutaBd);
+                cn.Open();
+                SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                adaptador.Fill(ds, "Productos");
+                grdProductos.DataSource = ds.Tables["Productos"];
+                grdProductos.DataBind();
+                cn.Close();
+            }
+            if(txtProducto.Text != "" && txtCategoria.Text!="")
+            {
+                if (ddlProducto.SelectedIndex == 0 && ddlCategoria.SelectedIndex==0)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto =" + idProducto + "and IdCategoría =" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 0)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría =" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 0 && ddlCategoria.SelectedIndex == 1)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto =" + idProducto + "and IdCategoría >" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 1)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría >" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 2 && ddlCategoria.SelectedIndex == 1)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto <" + idProducto + "and IdCategoría >" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 2)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría <" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlProducto.SelectedIndex == 2 && ddlCategoria.SelectedIndex == 2)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdProducto <" + idProducto + "and IdCategoría <" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+            }
+
+            if (txtProducto.Text != "" && txtCategoria.Text == "")
+            {
+                if (ddlProducto.SelectedIndex == 0)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string consulta = "select * from Productos where IdProducto =" + idProducto;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+                }
+                if (ddlProducto.SelectedIndex == 1)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string consulta = "select * from Productos where IdProducto >" + idProducto;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+                }
+                if (ddlProducto.SelectedIndex == 2)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idProducto = txtProducto.Text;
+                    string consulta = "select * from Productos where IdProducto <" + idProducto;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+                }
+
+
+            }
+            if (txtCategoria.Text != "" && txtProducto.Text=="")
+            {
+                if (ddlCategoria.SelectedIndex == 0)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdCategoría =" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlCategoria.SelectedIndex == 1)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdCategoría >" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+                if (ddlCategoria.SelectedIndex == 2)
+                {
+                    string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+                    string idCategoria = txtCategoria.Text;
+                    string consulta = "select * from Productos where IdCategoría <" + idCategoria;
+
+
+                    DataSet ds = new DataSet();
+                    SqlConnection cn = new SqlConnection(rutaBd);
+                    cn.Open();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+                    adaptador.Fill(ds, "Productos");
+                    grdProductos.DataSource = ds.Tables["Productos"];
+                    grdProductos.DataBind();
+                    cn.Close();
+
+                }
+            }
+            
+        }
+
+        protected void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtProducto.Text = "";
+            txtCategoria.Text = "";
+            string rutaBd = "Data;Initial Catalog=Neptuno;Integrated Security=True";
+
+            string consulta = "select * from Productos ";
+
+
+            DataSet ds = new DataSet();
+            SqlConnection cn = new SqlConnection(rutaBd);
+            cn.Open();
+            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
+
+            adaptador.Fill(ds, "Productos");
+            grdProductos.DataSource = ds.Tables["Productos"];
+            grdProductos.DataBind();
+            cn.Close();
+        }
+    }
+}
