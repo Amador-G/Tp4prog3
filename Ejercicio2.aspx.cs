b82c828cb052ce30c97b17a64745a212b591e6c4
Nicolas-Fediuk <nicolasfediuk@gmail.com>
2021-09-12 16:52:03 -0300

    Limpie un poco el cod ejer2

--- Ejercicio2.aspx.cs
+++ Ejercicio2.aspx.cs
@@ -14,11 +14,9 @@ namespace TP4_PROG3
 {
     public partial class Ejercicio2 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        public void consultaTabla(string consulta)
         {
             string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
-            string consulta = "select * from Productos ";
-
 
             DataSet ds = new DataSet();
             SqlConnection cn = new SqlConnection(rutaBd);
@@ -29,161 +27,97 @@ namespace TP4_PROG3
             grdProductos.DataSource = ds.Tables["Productos"];
             grdProductos.DataBind();
             cn.Close();
-
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            consultaTabla("select * from Productos ");
         }
 
         protected void btnFiltrar_Click(object sender, EventArgs e)
         {
-            if (txtProducto.Text=="" && txtCategoria.Text == "") 
+            if (txtProducto.Text == "" && txtCategoria.Text == "")
             {
-                string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
-
-                string consulta = "select * from Productos ";
 
 
-                DataSet ds = new DataSet();
-                SqlConnection cn = new SqlConnection(rutaBd);
-                cn.Open();
-                SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                adaptador.Fill(ds, "Productos");
-                grdProductos.DataSource = ds.Tables["Productos"];
-                grdProductos.DataBind();
-                cn.Close();
+                consultaTabla("select * from Productos ");
             }
             if(txtProducto.Text != "" && txtCategoria.Text!="")
             {
-                if (ddlProducto.SelectedIndex == 0 && ddlCategoria.SelectedIndex==0)
+                if (ddlProducto.SelectedIndex == 0 && ddlCategoria.SelectedIndex == 0)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto =" + idProducto + "and IdCategoría =" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto =" + idProducto + "and IdCategoría =" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
 
                 }
                 if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 0)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría =" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto >" + idProducto + "and IdCategoría =" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
 
                 }
                 if (ddlProducto.SelectedIndex == 0 && ddlCategoria.SelectedIndex == 1)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                   
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto =" + idProducto + "and IdCategoría >" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto =" + idProducto + "and IdCategoría >" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
 
                 }
                 if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 1)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría >" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto >" + idProducto + "and IdCategoría >" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
 
                 }
                 if (ddlProducto.SelectedIndex == 2 && ddlCategoria.SelectedIndex == 1)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                   
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto <" + idProducto + "and IdCategoría >" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto <" + idProducto + "and IdCategoría >" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
 
                 }
                 if (ddlProducto.SelectedIndex == 1 && ddlCategoria.SelectedIndex == 2)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                   
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto >" + idProducto + "and IdCategoría <" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto >" + idProducto + "and IdCategoría <" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                   
 
                 }
                 if (ddlProducto.SelectedIndex == 2 && ddlCategoria.SelectedIndex == 2)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+              
                     string idProducto = txtProducto.Text;
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdProducto <" + idProducto + "and IdCategoría <" + idCategoria;
+                    consultaTabla( "select * from Productos where IdProducto <" + idProducto + "and IdCategoría <" + idCategoria);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                  
 
                 }
             }
@@ -192,54 +126,30 @@ namespace TP4_PROG3
             {
                 if (ddlProducto.SelectedIndex == 0)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idProducto = txtProducto.Text;
-                    string consulta = "select * from Productos where IdProducto =" + idProducto;
+                    consultaTabla( "select * from Productos where IdProducto =" + idProducto);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
                 }
                 if (ddlProducto.SelectedIndex == 1)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idProducto = txtProducto.Text;
-                    string consulta = "select * from Productos where IdProducto >" + idProducto;
+                    consultaTabla( "select * from Productos where IdProducto >" + idProducto);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
                 }
                 if (ddlProducto.SelectedIndex == 2)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idProducto = txtProducto.Text;
-                    string consulta = "select * from Productos where IdProducto <" + idProducto;
+                    consultaTabla( "select * from Productos where IdProducto <" + idProducto);
 
 
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    
                 }
 
 
@@ -248,57 +158,23 @@ namespace TP4_PROG3
             {
                 if (ddlCategoria.SelectedIndex == 0)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdCategoría =" + idCategoria;
-
-
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
-
+                    consultaTabla("select * from Productos where IdCategoría =" + idCategoria);
+                    
                 }
                 if (ddlCategoria.SelectedIndex == 1)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                   
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdCategoría >" + idCategoria;
-
-
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
+                    consultaTabla( "select * from Productos where IdCategoría >" + idCategoria);
 
                 }
                 if (ddlCategoria.SelectedIndex == 2)
                 {
-                    string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+                    
                     string idCategoria = txtCategoria.Text;
-                    string consulta = "select * from Productos where IdCategoría <" + idCategoria;
-
-
-                    DataSet ds = new DataSet();
-                    SqlConnection cn = new SqlConnection(rutaBd);
-                    cn.Open();
-                    SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-                    adaptador.Fill(ds, "Productos");
-                    grdProductos.DataSource = ds.Tables["Productos"];
-                    grdProductos.DataBind();
-                    cn.Close();
-
+                    consultaTabla( "select * from Productos where IdCategoría <" + idCategoria);
                 }
             }
             
@@ -308,20 +184,9 @@ namespace TP4_PROG3
         {
             txtProducto.Text = "";
             txtCategoria.Text = "";
-            string rutaBd = "Data Source=LocalHost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True";
+            
 
-            string consulta = "select * from Productos ";
-
-
-            DataSet ds = new DataSet();
-            SqlConnection cn = new SqlConnection(rutaBd);
-            cn.Open();
-            SqlDataAdapter adaptador = new SqlDataAdapter(consulta, cn);
-
-            adaptador.Fill(ds, "Productos");
-            grdProductos.DataSource = ds.Tables["Productos"];
-            grdProductos.DataBind();
-            cn.Close();
+            consultaTabla( "select * from Productos ");
         }
     }
 }
